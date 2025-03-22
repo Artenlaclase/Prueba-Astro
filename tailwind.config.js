bc7068881886bb9d72f9b8bbc67a4be9f12b2a6e
Artenlaclase <artenlaclase@gmail.com>
2025-03-21 23:26:20 -0300

    error css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Asegúrate de incluir las rutas correctas
+      "./src/**/*.{astro,html,js,jsx,ts,tsx,vue}", // Asegúrate de incluir las rutas correctas
     ],
     theme: {
       extend: {},
